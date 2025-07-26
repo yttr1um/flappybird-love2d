8e2d48c6dbcbb133b694a0aba27460ad60e4cad6
Salem Alhaddad <85304055+yttr1um@users.noreply.github.com>
2025-07-26 20:12:03 +0400

    initial setup

---
+++ conf.lua
@@ -0,0 +1,5 @@
+function love.conf(t)
+    t.title = "Flappy Bird"
+    t.window.width = 720
+    t.window.height = 960
+end
---
+++ main.lua
@@ -0,0 +1,11 @@
+function love.load()
+
+end
+
+function love.update(dt)
+    
+end
+
+function love.draw()
+    love.graphics.setBackgroundColor(0.4, 0.6, 0.9)
+end
